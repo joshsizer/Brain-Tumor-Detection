564fe905f0f2a803fa77b97c45b953ecd59ffdb2
= <=>
2021-10-19 17:26:55 -0400

    Added documentation

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,41 @@
  * 5) Normalize input data
  */
 
+/**
+ * Detect brain tumors using a convolutional neural network.
+ * Can classify as having "no tumor," "glioma," "meningioma,"
+ * or "pituitary." The data set for this project comes from
+ * the Kaggle "Brain Tumor MRI Dataset," uploaded by user
+ * "Masoud Nickparavar."
+ *
+ * https://www.kaggle.com/masoudnickparvar/brain-tumor-mri-dataset
+ */
 import * as tf from "@tensorflow/tfjs-node";
-import path from "path";
 import BrainTumorModel from "./model";
 import BrainTumorData from "./data";
 import imagePreprocessor from "./preprocessor";
+import path from "path";
 
+// Define our data paths here, so it is clear where the data
+// is coming from. I've used path.join() to keep this platform
+// independent.
 const DATA_PATH = path.join(__dirname, "..", "data");
 const TRAINING_PATH = path.join(DATA_PATH, "Training");
 const TESTING_PATH = path.join(DATA_PATH, "Testing");
 const SAVE_MODEL_PATH = path.join(__dirname, "..", "model");
 
+// The preprocessor, model, and data generators need to know
+// the desired image shape. Ideally, the data generators should
+// not be dependant on the desired image shape; however, the
+// easiest way to coerce the input images to the correct number
+// of channels is via tf.node.decodeImage, which optionally
+// accepts the desired number of channels for the returned tensor.
+// While there is a tf.image.grayscaleToRGB function, there
+// is not a tf.image.RGBToGrayscale function at this moment.
+// The lack of ability to switch from 3 channels (RGB) to 1
+// channel (grayscale) means that the easiest place to define
+// the number of channels in our processed images is, unfortunately,
+// in the readImage function used by the data generators.
 const DESIRED_IMG_SHAPE = {
   height: 128,
   width: 128,
@@ -30,12 +54,24 @@ const brainTumorData = new BrainTumorData(
   DESIRED_IMG_SHAPE
 );
 
+// Pass in the desired image shape to allow for quick changes
+// in the model input shape. Helps for testing different model
+// architectures as well as tuning model performance.
 const model = new BrainTumorModel(DESIRED_IMG_SHAPE);
 
 const NUM_EPOCHS = 40;
 const BATCH_SIZE = 32;
+
+// Tensorflow is streaming images from disk instead of loading
+// them entirely into memory, so the shuffle process for shuffling
+// a batch of data must occur over a sample size larger than
+// the batch size.
 const SHUFFLE_BUFFER_SIZE = 100;
 
+// While a typical machine could load the entire dataset into
+// memory (~250 Mb), I opted to use stream generator functions
+// passed to tf.data.generator in case the dataset ever grows
+// in size.
 const xs = tf.data
   .generator(brainTumorData.data("train"))
   .map(imagePreprocessor(DESIRED_IMG_SHAPE));
--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,10 @@
 import * as tf from "@tensorflow/tfjs-node";
 
+/**
+ * Define a convolutional neural network by extending a
+ * tf.Sequential model. Observe the constructor for network's
+ * architecture.
+ */
 export default class BrainTumorModel extends tf.Sequential {
   constructor(inputImageShape: {
     height: number;
@@ -23,8 +28,6 @@ export default class BrainTumorModel extends tf.Sequential {
 
     super.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
 
-    // Repeat another conv2d + maxPooling stack.
-    // Note that we have more filters in the convolution.
     super.add(
       tf.layers.conv2d({
         kernelSize: 5,
@@ -34,10 +37,9 @@ export default class BrainTumorModel extends tf.Sequential {
         kernelInitializer: "varianceScaling",
       })
     );
+
     super.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
 
-    // Repeat another conv2d + maxPooling stack.
-    // Note that we have more filters in the convolution.
     super.add(
       tf.layers.conv2d({
         kernelSize: 5,
@@ -47,15 +49,11 @@ export default class BrainTumorModel extends tf.Sequential {
         kernelInitializer: "varianceScaling",
       })
     );
+
     super.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
 
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare
-    // it for input into our last layer. This is common practice when feeding
-    // higher dimensional data to a final classification output layer.
     super.add(tf.layers.flatten());
 
-    // Our last layer is a dense layer which has 10 output units, one for each
-    // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
     const NUM_OUTPUT_CLASSES = 4;
     super.add(
       tf.layers.dense({
@@ -65,8 +63,6 @@ export default class BrainTumorModel extends tf.Sequential {
       })
     );
 
-    // Choose an optimizer, loss function and accuracy metric,
-    // then compile and return the model
     const optimizer = tf.train.adam();
     super.compile({
       optimizer: optimizer,
